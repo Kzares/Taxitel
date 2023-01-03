b90269189cb6953334aa37e01d129d254e523f99
Kzares <96509059+Kzares@users.noreply.github.com>
2023-01-03 00:59:20 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { Footer, Navbar } from '../components';
 import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
 
 const Home = () => (
-  <div className="bg-primary-black home overflow-hidden">
+  <div className="home overflow-y-hidden overflow-x-hidden">
     <Navbar />
     <Hero />
     <div className="relative">
