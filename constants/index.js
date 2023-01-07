79dcd0c8518803a6b401f6695aa3f478ae6b9ebe
Kzares <96509059+Kzares@users.noreply.github.com>
2023-01-06 19:55:44 -0500

    Add files via upload

---
+++ constants/index.js
@@ -0,0 +1,39 @@
+
+import {FaTaxi} from 'react-icons/fa'
+import {AiOutlineTeam} from 'react-icons/ai'
+
+export const startingFeatures = [
+  {name:'chat.whatsapp.com/DDA...', url: 'https://chat.whatsapp.com/DDAJb9VWs9J03O3kaLp4gb'},
+  {name:'chat.whatsapp.com/D2u...', url: 'https://chat.whatsapp.com/D2uklm7aONb2kEHAHW6mUl'},
+  {name:'chat.whatsapp.com/CXg...', url: 'https://chat.whatsapp.com/CXgcJsi4ixPGmbxTNXkpsO'},  
+];
+
+export const newFeatures = [
+  {
+    icon: <AiOutlineTeam className='text-[40px] text-white' />,
+    title: 'Conductores a tu disposicion',
+    subtitle:
+      'Contamos con una flota de conductores bien preparados a su disposicion',
+  },
+  {
+    icon: <FaTaxi className='text-[40px]  text-white'/>,
+    title: 'Flexibilidad',
+    subtitle:
+      'Nuestro servicio es capaz de adaptarse a sus nesesidades',
+  },
+];
+
+
+export const socials = [
+
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+    src: 'https://instagram.com/taxitel_cuba?igshid=MWI4MTIyMDE='
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+    src:'https://www.facebook.com/taxitelcuba'
+  },
+];
